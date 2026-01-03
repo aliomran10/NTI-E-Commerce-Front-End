ab320317e4dbb27204f6690a7287a40d1eaf6526
Ali Omran <aliomran918273@gmail.com>
2026-01-03 20:39:38 +0200

    Modify url for deployment

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GlobalService {
-
-  hostName: string = 'http://localhost:3300';
+  hostName: string = 'https://e-commerce-express-ten.vercel.app';
   authRoute: string = '/api/v1/auth';
   productsRoute: string = '/api/v1/products';
   cartRoute: string = '/api/v1/carts';
@@ -13,7 +12,7 @@ export class GlobalService {
   wishlistRoute: string = '/api/v1/wishlist';
   reviewsRoute: string = '/api/v1/reviews';
   usersRoute: string = '/api/v1/users';
-  productsImages: string = `${this.hostName}/products/`
-  userImage: string = `${this.hostName}/users/`
-  constructor() { }
+  productsImages: string = `${this.hostName}/products/`;
+  userImage: string = `${this.hostName}/users/`;
+  constructor() {}
 }
